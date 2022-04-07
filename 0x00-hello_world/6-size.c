31d83f927c229e79a9e6f4db3c0c492d9280dd4e
abenet <solabenet@gmail.com>
2022-04-07 07:02:12 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char:", sizeof(char));
+printf("Size of int:" ,sizeof(int));
+printf("Size of long int:", sizeof(long int));
+printf("Size of long long int:" ,sizeof(long long int));
+printf("Size of float:" ,sizeof(float));
+return (0);
+}
