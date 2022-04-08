50a62b77d7997446b5868c81f2b98e74cfb82f5b
abenet <solabenet@gmail.com>
2022-04-08 05:21:24 -0700

    all tasks done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int combo;
+for (combo = 48; combo <= 57; combo++)
+{
+putchar(combo);
+
+if(combo == 57)
+{
+break;
+}
+
+else
+{
+putchar(',');
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
