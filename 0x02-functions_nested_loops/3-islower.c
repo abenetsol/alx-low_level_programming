230a66b9a42ad291863c436b932d2ce72e0de78d
abenet <solabenet@gmail.com>
2022-04-12 07:20:05 -0700

    task 3, 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - program than that checks for lowercase character
+ * @c: is the input variabl8e
+ * Only use _putchar
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+return (1);
+else
+return (0);
+}
