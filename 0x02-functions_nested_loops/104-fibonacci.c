49a417ca29257ff55c6d1d22bed27972f0f3795f
abenet <solabenet@gmail.com>
2022-04-12 08:51:37 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - main block
- * Description: Print Fibonacci sequence numbers up to a fib value
- * not exceeding 4,000,000.
- * Return: 0
+ * main - print the first 98 fibonacci numbers.
+ *
+ * Return: Nothing.
  */
+
 int main(void)
 {
-int i = 1, j = 2, total = 0;
-int k;
+int count;
+unsigned long x, y, z, a, b, c, carry;
 
-while (j < 4000000)
+x = 0;
+y = 1;
+
+for (count = 1; count <= 90; count++)
 {
-if (j % 2 == 0)
-total += j;
-
-k = j;
-j += i;
-i = k;
+z = x + y;
+x = y;
+y = z;
+printf("%lu, ", z);
 }
-printf("%d\n", total);
+
+a = x % 1000;
+x = x / 1000;
+b = y % 1000;
+y = y / 1000;
+
+while (count <= 98)
+{
+carry = (a + b) / 1000;
+c = (a + b) - carry * 1000;
+z = (x + y) + carry;
+x = y;
+y = z;
+a = b;
+b = c;
+
+if (c >= 100)
+printf("%lu%lu", z, c);
+else
+printf("%lu0%lu", z, c);
+if (count < 98)
+printf(", ");
+count++;
+}
+putchar('\n');
 return (0);
 }
