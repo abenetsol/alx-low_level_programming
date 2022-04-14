323937bce205508fe8db4f9c346bae4c6ecac905
abenet <solabenet@gmail.com>
2022-04-14 05:44:32 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,16 @@
  *print number - prints numbers
  *Return: Always 0.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 
 i = 0;
 while (i < 10)
 {
-
+if (i = 2 || i = 4)
+continue;
+else
 _putchar (i + '0');
 i++;
 }
