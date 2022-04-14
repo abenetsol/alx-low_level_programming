31c02b6ed8e27f65b0ff626d56873c06609440b2
abenet <solabenet@gmail.com>
2022-04-14 05:10:42 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiples input and outputs the result .
+ *@a: print int
+ *@b: print int
+ *Return: Always  0.
+ */
+
+int mul (int a, int b)
+{
+int multiple;
+multiple = a*b;
+return (multiple);
+}
