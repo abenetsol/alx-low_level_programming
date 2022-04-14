0830ac496bef94a15918f0643b71c09ca2076299
abenet <solabenet@gmail.com>
2022-04-14 01:20:36 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,13 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
-{
-putchar(i);
-i++;
+/*
+ *while (i < 10)
+ *{
+ *Infinite loop - No increase of variable
+ *putchar(i);
 }
-
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
