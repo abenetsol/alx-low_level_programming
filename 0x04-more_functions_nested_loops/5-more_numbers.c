c0b859226cbe48176f50530d74c56a485cd1b52d
abenet <solabenet@gmail.com>
2022-04-14 06:10:10 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers - print number
+ *Return: Always 0.
+ */
+void more_numbers(void)
+{
+
+int count,num;
+count = 0;
+while (count < 10)
+{
+num = 0
+while(num <= 14)
+{
+_putchar(num + '0');
+}
+_putchar('\n');
+}
+}
