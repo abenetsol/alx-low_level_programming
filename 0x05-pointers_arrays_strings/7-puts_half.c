1dc7142bd571308eaaa4aacda1ec0ba4ed9b3f0d
abenet <solabenet@gmail.com>
2022-04-18 13:50:17 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *put_half - print second half of the string
+ *@str: pointer that value changes
+ *
+ *Return: void meaning correct output
+ */
+vodi put_half(char *str)
+{
+int a, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar(str[i])
+}
+
+_putchar ('\n');
+
+}
