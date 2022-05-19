05d6306e7b32b4f184c2a80f6a413594a7b80f8c
abenet <solabenet@gmail.com>
2022-05-19 14:13:33 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,11 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
-
-    nelem = 0;
-    while (h)
-    {
-	    nelem++;
-	    h = h->next;
-    }
-    return (nelem);
+size_t nelem = 0;
+while (h)
+{
+nelem++;
+h = h->next;
+}
+return (nelem);
 }
