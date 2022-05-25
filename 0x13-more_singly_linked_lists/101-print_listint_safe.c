bead724cc7c8b9c01ea76fa38bce3b3e69dc8218
abenet <solabenet@gmail.com>
2022-05-25 13:17:12 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,12 +15,12 @@ while (head != NULL)
 {
 diff = head - head->next;
 num++;
-printf("[%p] %lu\n", (void *)head, head->n);
+printf("[%p] %d\n", (void *)head, head->n);
 if (diff > 0)
 head = head->next;
 else
 {
-printf("-> [%p] %lu\n", (void *)head->next, head->next->n);
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 break;
 }
 }
