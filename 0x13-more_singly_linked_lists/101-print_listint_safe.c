6a265c1e5f821e1d17670c10e84f86e8775f050f
abenet <solabenet@gmail.com>
2022-05-25 13:02:57 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,16 +11,16 @@ size_t print_listint_safe(const listint_t *head)
 size_t num = 0;
 long int diff;
 
-while (head)
+while (head != NULL)
 {
 diff = head - head->next;
 num++;
-printf("[%p] %u\n", (void *)head, head->n);
+printf("[%p] %d\n", (void *)head, head->n);
 if (diff > 0)
 head = head->next;
 else
 {
-printf("-> [%p] %u\n", (void *)head->next, head->next->n);
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 break;
 }
 }
